ed96040d5e6aaf0309f05fb8615ab78c40dc65ca
Hidalgo71 <y.ozdemir71@gmail.com>
2020-03-08 23:10:01 +0300

    Updated.

--- src/objectClassJava/calculateHouse.java
+++ src/objectClassJava/calculateHouse.java
@@ -17,6 +17,20 @@ public class calculateHouse
 
         double volume = homeObj.width * homeObj.length * homeObj.height;
         System.out.println("House's Volume is: " + volume);
-        
+
+        System.out.println("===================");
+
+        houseJava homeObj2;             //Empty Allocation
+        homeObj2 = new houseJava();     //Memory Allocated
+        homeObj2.height = 11;
+        homeObj2.length = 8;
+        homeObj2.width = 9;
+
+        System.out.println("House 2 Width: " + homeObj2.width);
+        System.out.println("House 2 Length: " + homeObj2.length);
+        System.out.println("House 2 Height: " + homeObj2.height);
+
+        double volume2 = homeObj2.width * homeObj2.length * homeObj2.height;
+        System.out.println("House's Volume is: " + volume2);
     }
 }
