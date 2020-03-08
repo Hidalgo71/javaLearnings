46587e42cac1934ea21ea18351bc8c051453189b
Hidalgo71 <y.ozdemir71@gmail.com>
2020-03-08 20:17:41 +0300

    Updated.

---
+++ src/javaLearningPack/nestedClass.java
@@ -0,0 +1,27 @@
+package javaLearningPack;
+
+public class nestedClass  //Class 1
+{
+    double width;
+    double length;
+    double height;
+}
+
+class calculate             //Class 2
+{
+    public static void main(String[] args)
+    {
+        objectJava homeObj = new objectJava();
+        homeObj.width = 7;
+        homeObj.length = 8;
+        homeObj.height = 5;
+
+        System.out.println("House Width: " + homeObj.width);
+        System.out.println("House Length: " + homeObj.length);
+        System.out.println("House Height: " + homeObj.height);
+
+        double volume = homeObj.width * homeObj.length * homeObj.height;
+        System.out.println("House's Volume is: " + volume);
+
+    }
+}
