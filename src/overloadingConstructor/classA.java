d53eee60c87b061f4d2725752924591ad16c8fe6
Hidalgo71 <y.ozdemir71@gmail.com>
2020-03-17 17:32:22 +0300

    Updated.

--- src/overloadingConstructor/classA.java
+++ src/overloadingConstructor/classA.java
@@ -5,6 +5,8 @@ public class classA
     double num1;
     double num2;
     double num3;
+    int var1;
+    int var2;
 
     public classA()         //non-parameter ready method
     {    }
@@ -14,6 +16,28 @@ public class classA
         this.num1 = num1;
         this.num2 = num2;
         this.num3 = num3;
+    }
+    public classA(int p1, int p2)
+    {
+        this.var1 = p1;
+        this.var2 = p2;
+    }
+
+    void sendValue(int num4, int num5)          //Call by Value, Sends Copy
+    {
+        num4 = num4 - 2;
+        num5 = num5 + 2;
+        System.out.println("sendValue Method inside the method..\n " +
+                "Num4: " + num4 + " Num5: " + num5);
+        //System.out.println("===================");
+    }
+
+    void sendObj(classA comingObj)              //Call by Reference, Sends original value
+    {
+        comingObj.var1 -= 5;
+        comingObj.var2 += 5;
+        System.out.println("sendObj Method inside the method..\n " +
+                "Var1: " + var1 + " var2: " + var2);
     }
 
     void test()
--- src/overloadingConstructor/classB.java
+++ src/overloadingConstructor/classB.java
@@ -4,9 +4,12 @@ public class classB
 {
     public static void main(String[] args)
     {
+
         classA aObj = new classA();
         double result;
         int iNum = 3;
+        int num4 = 14;
+        int num5 = 53;
 
         aObj.test();
         aObj.test(2);
@@ -20,5 +23,17 @@ public class classB
 
         classA aObj2 = new classA(10, 7, 1);
         aObj2.test();
+
+        classA obj1 = new classA();
+        System.out.println("Class B before values passed: " + num4 + " " + num5);
+        obj1.sendValue(num4, num5);
+        System.out.println("Class B after values passed: " + num4 + " " + num5);
+
+        System.out.println("===================");
+
+        classA obj2 = new classA(num4, num5);
+        System.out.println("Class B before object passed: " + obj2.var1 + " " + obj2.var2);
+        obj2.sendObj(obj2);
+        System.out.println("Class B after object passed: " + obj2.var1 + " " + obj2.var2);
     }
 }
